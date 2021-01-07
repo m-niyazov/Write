53678c4f7cf523d19f916a78d8e53434b79a1d44
Muhamed <niyazov_16@mail.ru>
2021-01-07 19:22:19 +0300

    Initial SingleViewApp

---
+++ Write_/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Write_
+//
+//  Created by Muhamed Niyazov on 07.01.2021.
+//  Copyright © 2021 Muhamed Niyazov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
